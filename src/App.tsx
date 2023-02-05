00863f6838aa0b1f2df0f1ebdf2712b8fe6ae1bd
MrPancakes39 <salmannhassan39@gmail.com>
2023-02-05 19:23:47 +0200

    Basis for todo container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
 
+function TodoContainer() {
+  return (
+    <div className="todo-container">
+      <p>Hi</p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="app">
       <div className="background"></div>
+      <TodoContainer />
     </div>
   );
 }

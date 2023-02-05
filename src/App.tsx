d4fb5e77d08856605ee40092297efc111483ebaa
MrPancakes39 <salmannhassan39@gmail.com>
2023-02-05 20:14:06 +0200

    Can now add a todo item

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Todo from "./Todo";
 
 function TodoContainer() {
-  const todos = ["Make food", "Play OuterWilds"];
+  let [todos, setTodos] = useState<string[]>([]);
   return (
     <form className="todo-container">
       <h4 className="title">Cool Todo App</h4>
       <div className="form-group">
-        <input placeholder="Add a todo item" />
-        <button type="button">+</button>
+        <input placeholder="Add a todo item" id="todo_input" />
+        <button type="button" onClick={() => setTodos([...todos, "todo item"])}>
+          +
+        </button>
       </div>
       <div className="todos">
-        {todos.map((val) => (
-          <Todo todo={val} />
+        {todos.map((val, i) => (
+          <Todo key={i} todo={val} />
         ))}
       </div>
     </form>

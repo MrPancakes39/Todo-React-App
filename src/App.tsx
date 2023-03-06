5ca282ac5198798e5b6f5cd4bae16c875b658475
MrPancakes39 <selenahasan39@proton.me>
2023-03-06 16:33:11 +0200

    Refactored click to a seperate function

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,22 @@ function TodoContainer() {
   let [todos, setTodos] = useState<TodoItem[]>([]);
   let inputRef = useRef<HTMLInputElement>(null);
 
+  function addToList() {
+    const elt = inputRef.current as HTMLInputElement;
+    const item = elt.value;
+    if (item === "") alert("Can't add empty item.");
+    else {
+      setTodos([...todos, { text: item, key: (todos.slice(-1)[0]?.key ?? 0) + 1 }]);
+      elt.value = "";
+    }
+  }
+
   return (
     <form className="todo-container">
       <h4 className="title">Cool Todo App</h4>
       <div className="form-group">
         <input placeholder="Add a todo item" id="todo_input" ref={inputRef} />
-        <button
-          type="button"
-          onClick={() => {
-            const elt = inputRef.current as HTMLInputElement;
-            const item = elt.value;
-            if (item === "") alert("Can't add empty item.");
-            else {
-              setTodos([...todos, { text: item, key: (todos.slice(-1)[0]?.key ?? 0) + 1 }]);
-              elt.value = "";
-            }
-          }}
-        >
+        <button type="button" onClick={addToList}>
           +
         </button>
       </div>
